5d3b58b28964fcdd7e0894e0c8416db9248c26a7
khaoula karoui <khaoula11karoui@gmail.com>
2024-07-19 20:51:25 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { CourseComponent } from './components/course/course.component';
+import { TeacherComponent } from './components/teacher/teacher.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    BannerComponent,
+    TestimonialComponent,
+    BlogComponent,
+    CourseComponent,
+    TeacherComponent,
+    CoursesComponent,
+    TeachersComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
